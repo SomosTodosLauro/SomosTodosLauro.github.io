0dea3e6ff428c65edfa62d8cfb8f98bc2eca03a1
Victor Silva <victor.psilva@viavarejo.com.br>
2021-04-08 21:39:01 -0300

    adding mouthes

--- src/app/lauro/options/mouth.enum.ts
+++ src/app/lauro/options/mouth.enum.ts
@@ -3,5 +3,7 @@ export enum Mouth {
   FELIZ = 'mouth-2',
   LINGUA = 'mouth-3',
   TRISTE = 'mouth-4',
-  PENSATIVO = 'mouth-5'
+  PENSATIVO = 'mouth-5',
+  CHATEADO = 'mouth-6',
+  SORRIDENTE = 'mouth-7'
 }
