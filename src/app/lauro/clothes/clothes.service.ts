0b9990998564cc7d68856b41999a6249a9fbf3e9
MrTalaris <kevintalarico@hotmail.com>
2021-04-10 16:57:15 -0300

    Clothes colors + 3 unconventional hairs

--- src/app/lauro/clothes/clothes.service.ts
+++ src/app/lauro/clothes/clothes.service.ts
@@ -7,6 +7,16 @@ import { OptionsService } from '../services/options.service';
 })
 export class ClothesService extends OptionsService {
 
+  colors = [
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-1
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-2
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-3
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-4
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-5
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-6
+    ['#ffc50c', '#49a8de', '#ed1c24', '#d6d5d4', '#3d3d3d'], // Clothes-7
+  ];
+
   constructor() {
     super(Clothes)
    }
--- src/app/lauro/hair/hair.service.ts
+++ src/app/lauro/hair/hair.service.ts
@@ -8,14 +8,14 @@ import { OptionsService } from '../services/options.service';
 export class HairService extends OptionsService {
 
   colors = [
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-3
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-3
     [], // hair-1
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-2
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-4
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-5
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-6
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-7
-    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF'], // hair-8
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-2
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-4
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-5
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-6
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-7
+    ['#000', '#E2D343', '#6F3C1F', '#CC3300', '#CFCFCF', '#007bd3', '#00d16f', '#E83691'], // hair-8
   ];
 
   constructor() {
